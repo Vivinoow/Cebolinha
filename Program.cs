b794c260f978e43f73df9588703fac03567019b1
Viviane <vpereiradasilva555@gmail.com>
2022-09-17 10:04:52 -0300

    Cebolinha

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string fraseDigitada, fraseComDislalia;
+
+Console.Write("Digite uma flase: ");
+fraseDigitada = Console.ReadLine()!;
+
+fraseComDislalia = fraseDigitada
+        .Replace("r", "1")
+        .Replace("R", "L");
+
+Console.WriteLine(fraseComDislalia);
+
